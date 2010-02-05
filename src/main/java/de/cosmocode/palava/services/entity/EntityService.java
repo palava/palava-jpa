5bd5770030d326e5e8110615bf6fd275ed622642
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-02-05 18:03:39 +0000

    added missing javadocs, organized imports

--- src/main/java/de/cosmocode/palava/services/entity/EntityService.java
+++ src/main/java/de/cosmocode/palava/services/entity/EntityService.java
@@ -58,7 +58,7 @@ public interface EntityService<T extends EntityBase> extends Service {
      * Retrievs an entity from the database.
      * 
      * @param query the query being executed
-     * @param parameters the parameters this query needs
+     * @param parameters the parameters the query needs
      * @return the single entity found using the given query
      * @throws EntityNotFoundException if there is no matching entity
      * @throws NonUniqueResultException if there is more than one matching entity
@@ -69,7 +69,7 @@ public interface EntityService<T extends EntityBase> extends Service {
      * Retrievs an entity from the database.
      * 
      * @param queryName the name of the query being executed
-     * @param parameters the parameters this query needs
+     * @param parameters the parameters the query needs
      * @return the single entity found using the given query
      * @throws EntityNotFoundException if there is no matching entity
      * @throws NonUniqueResultException if there is more than one matching entity
@@ -91,7 +91,7 @@ public interface EntityService<T extends EntityBase> extends Service {
      * Retrievs a list of entities from the database.
      * 
      * @param query the query being executed
-     * @param parameters the parameters this query needs
+     * @param parameters the parameters the query needs
      * @return the single entity found using the given query
      */
     List<T> list(Query query, Object... parameters);
@@ -100,7 +100,7 @@ public interface EntityService<T extends EntityBase> extends Service {
      * Retrievs a list of entities from the database.
      * 
      * @param queryName the name of the query being executed
-     * @param parameters the parameters this query needs
+     * @param parameters the parameters the query needs
      * @return the single entity found using the given query
      */
     List<T> list(String queryName, Object... parameters);
@@ -147,27 +147,99 @@ public interface EntityService<T extends EntityBase> extends Service {
      * 
      * @param <P> the generic value type
      * @param query the query being executed
-     * @param parameters the parameters this query needs
+     * @param parameters the parameters the query needs
      * @return the value found by the given query
      */
     <P> P projection(Query query, Object... parameters);
     
+    /**
+     * Retrieves a projection, a single column, single row value.
+     * 
+     * @param <P> the generic value type
+     * @param queryName the name of the query being executed
+     * @param parameters the parameters the query needs
+     * @return the value found by the given query
+     */
     <P> P projection(String queryName, Object... parameters);
     
+    /**
+     * Retrieves projections, multiple columns, single row values.
+     * 
+     * @param <P> the generic value type
+     * @param query the query being executed
+     * @param parameters the parameters the query need
+     * @return the values found by the given query
+     */
     <P> P[] projections(Query query, Object... parameters);
-    
+
+    /**
+     * Retrieves projections, multiple columns, single row values.
+     * 
+     * @param <P> the generic value type
+     * @param queryName the name of the query being executed
+     * @param parameters the parameters the query need
+     * @return the values found by the given query
+     */
     <P> P[] projections(String queryName, Object... parameters);
     
+    /**
+     * Retrieves a list of projections, single column, multiple row values.
+     * 
+     * @param <P> the generic value type
+     * @param query the query being executed
+     * @param parameters the parameters the query needs
+     * @return the values found by the given query
+     */
     <P> List<P> projectionList(Query query, Object... parameters);
-    
+
+    /**
+     * Retrieves a list of projections, single column, multiple row values.
+     * 
+     * @param <P> the generic value type
+     * @param queryName the name of the query being executed
+     * @param parameters the parameters the query needs
+     * @return the values found by the given query
+     */
     <P> List<P> projectionList(String queryName, Object... parameters);
     
+    /**
+     * Retrieves a list of projections, multiple columns, multiple row values.
+     * 
+     * @param <P> the generic value type
+     * @param query the query being executed
+     * @param parameters the parameters the query needs
+     * @return the values found by the given query
+     */
     <P> List<P[]> projectionsList(Query query, Object... parameters);
-    
+
+    /**
+     * Retrieves a list of projections, multiple columns, multiple row values.
+     * 
+     * @param <P> the generic value type
+     * @param queryName the name of the query being executed
+     * @param parameters the parameters the query needs
+     * @return the values found by the given query
+     */
     <P> List<P[]> projectionsList(String queryName, Object... parameters);
     
-    Query prepare(String queryName, Object... parameters);
-    
+    /**
+     * Prepares the specified query with the given parameters by calling
+     * {@link Query#setParameter(int, Object)} for every parameter.
+     * 
+     * @param query the query being prepared
+     * @param parameters the parameters the query requires
+     * @return the specified query
+     */
     Query prepare(Query query, Object... parameters);
+    
+    /**
+     * Prepares the query associated with the specified name with the given parameters
+     * by calling {@link Query#setParameter(int, Object)} for every parameter.
+     * 
+     * @param queryName the name of the query being prepared
+     * @param parameters the parameters the query requires
+     * @return the prepared query
+     */
+    Query prepare(String queryName, Object... parameters);
     
 }
--- src/main/java/de/cosmocode/palava/services/persistence/DefaultPersistenceService.java
+++ src/main/java/de/cosmocode/palava/services/persistence/DefaultPersistenceService.java
@@ -29,7 +29,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.inject.Inject;
-import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 import com.google.inject.servlet.RequestScoped;
 
